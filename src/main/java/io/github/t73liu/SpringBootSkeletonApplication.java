24ba4e24310eaba1dc26d34ace88b055c3ce1643
t73liu <t73liu@gmail.com>
2017-05-06 23:48:46 -0400

    Initial commit

---
+++ src/main/java/io/github/t73liu/SpringBootSkeletonApplication.java
@@ -0,0 +1,12 @@
+package io.github.t73liu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSkeletonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSkeletonApplication.class, args);
+	}
+}
