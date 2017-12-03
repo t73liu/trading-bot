dcd002ad224f946e81aded35176b5a5ff59a63bb
t73liu <t73liu@gmail.com>
2017-12-02 21:28:20 -0500

    Add caffeine caching

--- src/main/java/io/github/t73liu/TradingApplication.java
+++ src/main/java/io/github/t73liu/TradingApplication.java
@@ -2,10 +2,12 @@ package io.github.t73liu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 public class TradingApplication {
     public static void main(String[] args) {
         SpringApplication.run(TradingApplication.class, args);
