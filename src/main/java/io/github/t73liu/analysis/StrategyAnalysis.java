cf43952c116946acf4fb975f64bf02048ad62816
t73liu <t73liu@gmail.com>
2017-12-06 17:00:35 -0500

    Additional strategy evaluation criteria

---
+++ src/main/java/io/github/t73liu/analysis/StrategyAnalysis.java
@@ -0,0 +1,36 @@
+package io.github.t73liu.analysis;
+
+import it.unimi.dsi.fastutil.objects.Object2DoubleLinkedOpenHashMap;
+import org.ta4j.core.*;
+import org.ta4j.core.analysis.criteria.*;
+
+import java.util.Map;
+
+public class StrategyAnalysis {
+    private static final AnalysisCriterion AVERAGE_PROFIT_CRITERION = new AverageProfitCriterion();
+    private static final AnalysisCriterion AVERAGE_PROFITABLE_TRADES_CRITERION = new AverageProfitableTradesCriterion();
+    private static final AnalysisCriterion BUY_HOLD_CRITERION = new BuyAndHoldCriterion();
+    private static final AnalysisCriterion MAX_DRAWDOWN_CRITERION = new MaximumDrawdownCriterion();
+    private static final AnalysisCriterion NUMBER_BARS_CRITERION = new NumberOfBarsCriterion();
+    private static final AnalysisCriterion NUMBER_TRADES_CRITERION = new NumberOfTradesCriterion();
+    private static final AnalysisCriterion REWARD_RISK_RATIO_CRITERION = new RewardRiskRatioCriterion();
+    private static final AnalysisCriterion PROFIT_CRITERION = new TotalProfitCriterion();
+    private static final int TOTAL_CRITERION_COUNT = 9;
+    private static final int INITIAL_AMOUNT = 1;
+
+    public static Map<String, Double> analyze(TimeSeries series, Strategy strategy, double relativeTransactionFee, double flatTransactionFee) {
+        TradingRecord tradingRecord = new TimeSeriesManager(series).run(strategy);
+        Map<String, Double> analysisMap = new Object2DoubleLinkedOpenHashMap<>(TOTAL_CRITERION_COUNT);
+        AnalysisCriterion transactionCostCriterion = new LinearTransactionCostCriterion(INITIAL_AMOUNT, relativeTransactionFee, flatTransactionFee);
+        analysisMap.put("AVERAGE_PROFIT_CRITERION", AVERAGE_PROFIT_CRITERION.calculate(series, tradingRecord));
+        analysisMap.put("AVERAGE_PROFITABLE_TRADES_CRITERION", AVERAGE_PROFITABLE_TRADES_CRITERION.calculate(series, tradingRecord));
+        analysisMap.put("BUY_HOLD_CRITERION", BUY_HOLD_CRITERION.calculate(series, tradingRecord));
+        analysisMap.put("MAX_DRAWDOWN_CRITERION", MAX_DRAWDOWN_CRITERION.calculate(series, tradingRecord));
+        analysisMap.put("NUMBER_BARS_CRITERION", NUMBER_BARS_CRITERION.calculate(series, tradingRecord));
+        analysisMap.put("NUMBER_TRADES_CRITERION", NUMBER_TRADES_CRITERION.calculate(series, tradingRecord));
+        analysisMap.put("REWARD_RISK_RATIO_CRITERION", REWARD_RISK_RATIO_CRITERION.calculate(series, tradingRecord));
+        analysisMap.put("TRANSACTION_COST_CRITERION", transactionCostCriterion.calculate(series, tradingRecord));
+        analysisMap.put("PROFIT_CRITERION", PROFIT_CRITERION.calculate(series, tradingRecord) - analysisMap.getOrDefault("TRANSACTION_COST_CRITERION", 0d));
+        return analysisMap;
+    }
+}
--- src/test/java/io/github/t73liu/analysis/StrategyAnalysisTest.java
+++ src/test/java/io/github/t73liu/analysis/StrategyAnalysisTest.java
@@ -1,24 +1,26 @@
-package io.github.t73liu.calc;
+package io.github.t73liu.analysis;
 
 import io.github.t73liu.model.poloniex.PoloniexCandle;
 import io.github.t73liu.strategy.trading.CandleStrategy;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
-import org.apache.commons.math3.util.Precision;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.ta4j.core.*;
+import org.ta4j.core.Bar;
+import org.ta4j.core.BaseTimeSeries;
+import org.ta4j.core.Strategy;
+import org.ta4j.core.TimeSeries;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static io.github.t73liu.util.MapperUtil.readCSV;
-import static io.github.t73liu.util.MathUtil.roundDecimalToDouble;
 
-public class EarningsCalculatorTest {
-    private static final Logger LOGGER = LoggerFactory.getLogger(EarningsCalculatorTest.class);
+public class StrategyAnalysisTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(StrategyAnalysisTest.class);
 
     private static Stream<String> pathProvider() {
         return Stream.of("src/test/resources/USDT_ETH.300.poloniex.csv", "src/test/resources/USDT_ETH.900.poloniex.csv",
@@ -29,17 +31,15 @@ public class EarningsCalculatorTest {
 
     @ParameterizedTest
     @MethodSource("pathProvider")
-    public void testStrategyProfitability(String path) throws Exception {
+    public void testStrategyAnalysis(String path) throws Exception {
         List<Bar> ticks = readCSV(PoloniexCandle.class, path).stream()
                 .map(PoloniexCandle::toTick)
                 .collect(Collectors.toCollection(ObjectArrayList::new));
         TimeSeries series = new BaseTimeSeries(ticks);
         Strategy strategy = CandleStrategy.getStrategy(series);
-        double takerFee = 0.002;
-        double profit = EarningsCalculator.calculateProfit(series, strategy, takerFee);
-        Decimal growth = ticks.get(ticks.size() - 1).getClosePrice().dividedBy(ticks.get(0).getClosePrice());
-        LOGGER.info("DEFAULT: {}, STRATEGY:{}", roundDecimalToDouble(growth), Precision.round(profit, 8));
-        // RE-ENABLE TEST WHEN STRATEGY SOUND
-//        Assertions.assertTrue(profit > growth.multipliedBy(Decimal.valueOf(1.05)).doubleValue());
+        double relativeFee = 0.002;
+        double flatFee = 0d;
+        Map<String, Double> analysis = StrategyAnalysis.analyze(series, strategy, relativeFee, flatFee);
+        LOGGER.info("Data: {}, Analysis: {}", path, analysis);
     }
 }
