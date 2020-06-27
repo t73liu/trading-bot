0d5f9d263df31766f507fdd6acee147d89f13e70
Tao <t73liu@gmail.com>
2020-06-27 04:01:16 -0700

    refactor: Remove duplicated code

--- jobs/daily-stock-news/main.go
+++ jobs/daily-stock-news/main.go
@@ -12,21 +12,27 @@ import (
 // upcoming IPOs and Earnings
 func main() {
 	client := yahoo.NewClient(&http.Client{Timeout: 15 * time.Second})
-	earnings, err := client.GetEarningsCall("", "")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	for _, earningsCall := range earnings {
-		fmt.Printf("%+v\n", earningsCall)
-	}
+	now := time.Now()
 
-	ipos, err := client.GetIPOs("", "")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	for _, ipo := range ipos {
-		fmt.Printf("%+v\n", ipo)
+	// Get Earnings/IPOs for the next 2 weeks
+	for days := 0; days < 14; days++ {
+		date := now.AddDate(0, 0, days)
+		earnings, err := client.GetEarningsCall(date)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		for _, earningsCall := range earnings {
+			fmt.Printf("%+v\n", earningsCall)
+		}
+
+		ipos, err := client.GetIPOs(date)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		for _, ipo := range ipos {
+			fmt.Printf("%+v\n", ipo)
+		}
 	}
 }
--- lib/yahoo-stock-calendar/client.go
+++ lib/yahoo-stock-calendar/client.go
@@ -5,7 +5,10 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"time"
 )
+
+const baseURL = "https://finance.yahoo.com/calendar/"
 
 type EarningsCallTime string
 
@@ -46,61 +49,68 @@ func NewClient(httpClient *http.Client) *Client {
 	}
 }
 
-func (c *Client) GetEarningsCall(startDate, endDate string) (earnings []EarningsCall, err error) {
-	response, err := c.client.Get("https://finance.yahoo.com/calendar/earnings?from=2020-06-28&to=2020-07-04&day=2020-07-02")
+func (c *Client) GetEarningsCall(date time.Time) (earnings []EarningsCall, err error) {
+	rows, err := c.getEvents("earnings", date)
 	if err != nil {
 		return earnings, err
 	}
-	defer response.Body.Close()
-
-	bodyBytes, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return earnings, err
-	}
-
-	bodyString := string(bodyBytes)
-	for _, str := range strings.Split(bodyString, "\n") {
-		length := len(str)
-		if length > 16 && str[:16] == "root.App.main = " {
-			var data map[string]interface{}
-			if err = json.Unmarshal([]byte(str[16:length-1]), &data); err != nil {
-				return earnings, err
-			}
-			context := data["context"].(map[string]interface{})
-			dispatcher := context["dispatcher"].(map[string]interface{})
-			stores := dispatcher["stores"].(map[string]interface{})
-			resultsStore := stores["ScreenerResultsStore"].(map[string]interface{})
-			results := resultsStore["results"].(map[string]interface{})
-			rows := results["rows"].([]interface{})
-			for _, row := range rows {
-				record := row.(map[string]interface{})
-				earningsCall := EarningsCall{
-					Ticker:             record["ticker"].(string),
-					Company:            record["companyshortname"].(string),
-					StartTime:          EarningsCallTime(record["startdatetimetype"].(string)),
-					EPSEstimate:        getFloat64(record["epsestimate"]),
-					EPSActual:          getFloat64(record["epsactual"]),
-					EPSSurprisePercent: getFloat64(record["epssurprisepct"]),
-					QuoteType:          record["quoteType"].(string),
-				}
-				earnings = append(earnings, earningsCall)
-			}
-			break
+	for _, row := range rows {
+		record := row.(map[string]interface{})
+		earningsCall := EarningsCall{
+			Ticker:             record["ticker"].(string),
+			Company:            record["companyshortname"].(string),
+			StartTime:          EarningsCallTime(record["startdatetimetype"].(string)),
+			EPSEstimate:        getFloat64(record["epsestimate"]),
+			EPSActual:          getFloat64(record["epsactual"]),
+			EPSSurprisePercent: getFloat64(record["epssurprisepct"]),
+			QuoteType:          record["quoteType"].(string),
 		}
+		earnings = append(earnings, earningsCall)
 	}
 	return earnings, nil
 }
 
-func (c *Client) GetIPOs(startDate, endDate string) (ipos []IPO, err error) {
-	response, err := c.client.Get("https://finance.yahoo.com/calendar/ipo?from=2020-06-28&to=2020-07-04&day=2020-07-02")
+func (c *Client) GetIPOs(date time.Time) (ipos []IPO, err error) {
+	rows, err := c.getEvents("ipo", date)
 	if err != nil {
 		return ipos, err
+	}
+	for _, row := range rows {
+		record := row.(map[string]interface{})
+		ipo := IPO{
+			Ticker:    record["ticker"].(string),
+			Company:   record["companyshortname"].(string),
+			Exchange:  record["exchange_short_name"].(string),
+			PriceFrom: getFloat64(record["pricefrom"]),
+			PriceTo:   getFloat64(record["priceto"]),
+			Currency:  record["currencyname"].(string),
+			QuoteType: record["quoteType"].(string),
+		}
+		ipos = append(ipos, ipo)
+	}
+	return ipos, err
+}
+
+func (c *Client) getEvents(eventType string, date time.Time) ([]interface{}, error) {
+	req, err := http.NewRequest("GET", baseURL+eventType, nil)
+	if err != nil {
+		return nil, err
+	}
+	queryParams := req.URL.Query()
+	if !date.IsZero() {
+		queryParams.Add("day", formatISO(date))
+	}
+	req.URL.RawQuery = queryParams.Encode()
+
+	response, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
 	}
 	defer response.Body.Close()
 
 	bodyBytes, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		return ipos, err
+		return nil, err
 	}
 
 	bodyString := string(bodyBytes)
@@ -109,7 +119,7 @@ func (c *Client) GetIPOs(startDate, endDate string) (ipos []IPO, err error) {
 		if length > 16 && str[:16] == "root.App.main = " {
 			var data map[string]interface{}
 			if err = json.Unmarshal([]byte(str[16:length-1]), &data); err != nil {
-				return ipos, err
+				return nil, err
 			}
 			context := data["context"].(map[string]interface{})
 			dispatcher := context["dispatcher"].(map[string]interface{})
@@ -117,23 +127,14 @@ func (c *Client) GetIPOs(startDate, endDate string) (ipos []IPO, err error) {
 			resultsStore := stores["ScreenerResultsStore"].(map[string]interface{})
 			results := resultsStore["results"].(map[string]interface{})
 			rows := results["rows"].([]interface{})
-			for _, row := range rows {
-				record := row.(map[string]interface{})
-				ipo := IPO{
-					Ticker:    record["ticker"].(string),
-					Company:   record["companyshortname"].(string),
-					Exchange:  record["exchange_short_name"].(string),
-					PriceFrom: getFloat64(record["pricefrom"]),
-					PriceTo:   getFloat64(record["priceto"]),
-					Currency:  record["currencyname"].(string),
-					QuoteType: record["quoteType"].(string),
-				}
-				ipos = append(ipos, ipo)
-			}
-			break
+			return rows, nil
 		}
 	}
-	return ipos, nil
+	return nil, nil
+}
+
+func formatISO(date time.Time) string {
+	return date.Format("2006-01-02")
 }
 
 func getFloat64(val interface{}) *float64 {
