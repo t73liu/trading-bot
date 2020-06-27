1bddda52b626fc8c5c61416dbe3dbfb2870f6fb8
Tao <t73liu@gmail.com>
2020-06-27 04:38:43 -0700

    feat: Get ticker details from Polygon

--- lib/polygon/client.go
+++ lib/polygon/client.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"net/http"
 	"strconv"
-	"time"
 )
 
 const polygonHost = "https://api.polygon.io"
@@ -22,21 +21,6 @@ func NewClient(httpClient *http.Client, apiKey string) *Client {
 	}
 }
 
-type TickersResponse struct {
-	Count   int      `json:"count"`
-	Tickers []Ticker `json:"tickers"`
-}
-
-type Ticker struct {
-	Ticker          string `json:"ticker"`
-	Name            string `json:"name"`
-	Market          string `json:"market"`
-	Locale          string `json:"locale"`
-	Currency        string `json:"currency"`
-	Active          bool   `json:"active"`
-	PrimaryExchange string `json:"primaryExch"`
-}
-
 type TickersQueryParams struct {
 	PerPage int
 	Page    int
@@ -46,14 +30,6 @@ type TickersQueryParams struct {
 	Locale  string
 	Search  string
 	Sort    string
-}
-
-type Article struct {
-	Title     string    `json:"title"`
-	URL       string    `json:"url"`
-	Source    string    `json:"source"`
-	Summary   string    `json:"summary"`
-	Timestamp time.Time `json:"timestamp"`
 }
 
 func (c *Client) GetTickers(params TickersQueryParams) (result TickersResponse, err error) {
@@ -109,4 +85,27 @@ func (c *Client) GetTickerNews(ticker string, perPage int, page int) (articles [
 		return articles, err
 	}
 	return articles, nil
+}
+
+func (c *Client) GetTickerDetails(ticker string) (detail TickerDetails, err error) {
+	url := polygonHost + "/v1/meta/symbols/" + ticker + "/company"
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return detail, err
+	}
+	queryParams := req.URL.Query()
+	queryParams.Add("apiKey", c.apiKey)
+	req.URL.RawQuery = queryParams.Encode()
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return detail, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return detail, errors.New("Response failed with status code: " + resp.Status)
+	}
+	if err := json.NewDecoder(resp.Body).Decode(&detail); err != nil {
+		return detail, err
+	}
+	return detail, nil
 }
---
+++ lib/polygon/types.go
@@ -0,0 +1,40 @@
+package polygon
+
+import "time"
+
+type TickersResponse struct {
+	Count   int      `json:"count"`
+	Tickers []Ticker `json:"tickers"`
+}
+
+type Ticker struct {
+	Ticker          string `json:"ticker"`
+	Name            string `json:"name"`
+	Market          string `json:"market"`
+	Locale          string `json:"locale"`
+	Currency        string `json:"currency"`
+	Active          bool   `json:"active"`
+	PrimaryExchange string `json:"primaryExch"`
+}
+
+type Article struct {
+	Title     string    `json:"title"`
+	URL       string    `json:"url"`
+	Source    string    `json:"source"`
+	Summary   string    `json:"summary"`
+	Timestamp time.Time `json:"timestamp"`
+}
+
+type TickerDetails struct {
+	Symbol      string   `json:"symbol"`
+	Name        string   `json:"name"`
+	Exchange    string   `json:"exchange"`
+	Country     string   `json:"country"`
+	Industry    string   `json:"industry"`
+	Sector      string   `json:"sector"`
+	Description string   `json:"description"`
+	URL         string   `json:"url"`
+	MarketCap   int      `json:"marketcap"`
+	Similar     []string `json:"similar"`
+	Tags        []string `json:"tags"`
+}
