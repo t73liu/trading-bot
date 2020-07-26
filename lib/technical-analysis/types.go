798ab6b05fa3df3c1786d582e876f3e3af837d2e
Tao <t73liu@gmail.com>
2020-07-25 23:55:30 -0400

    feat: Add custom json marshalling for indicators

--- lib/technical-analysis/types.go
+++ lib/technical-analysis/types.go
@@ -1,11 +1,27 @@
 package analyze
 
+import "encoding/json"
+
 type ValidMicro struct {
 	Micro int64
 	Valid bool
 }
 
+func (vm ValidMicro) MarshalJson() ([]byte, error) {
+	if vm.Valid {
+		return json.Marshal(MicrosToDollars(vm.Micro))
+	}
+	return json.Marshal(nil)
+}
+
 type ValidFloat struct {
 	Value float64
 	Valid bool
+}
+
+func (vf ValidFloat) MarshalJson() ([]byte, error) {
+	if vf.Valid {
+		return json.Marshal(vf.Value)
+	}
+	return json.Marshal(nil)
 }
