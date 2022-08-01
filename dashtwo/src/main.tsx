0401a02309bdf20adeec7d7173c3f97bc7416b68
t73liu <t73liu@gmail.com>
2022-08-01 00:44:15 -0400

    feat: Add redux toolkit

--- dashtwo/src/main.tsx
+++ dashtwo/src/main.tsx
@@ -1,10 +1,16 @@
+import "./index.css";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+
 import App from "./App";
-import "./index.css";
+import { store } from "./state/store";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
