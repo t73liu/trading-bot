a06495aa609cfc47a3c63cdc4d44b7969a8c1659
t73liu <t73liu@gmail.com>
2022-07-31 23:28:55 -0400

    feat: Add eslint and prettier

--- dashtwo/src/App.tsx
+++ dashtwo/src/App.tsx
@@ -1,11 +1,9 @@
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!!
-      </h1>
+      <h1 className="text-3xl font-bold underline">Hello world!!</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
