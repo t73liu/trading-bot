6582796452fb64535862c5b67e259f49270725a8
t73liu <t73liu@gmail.com>
2022-07-31 22:02:16 -0400

    feat: Add tailwind and daisy UI

--- dashtwo/src/App.tsx
+++ dashtwo/src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div className="App">
-      <h1>Placeholder</h1>
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!!
+      </h1>
     </div>
   )
 }
